56e083fffe942e0a65d4353b0518c62c25c83080
Syed-Ibrahim786 <84724836+Syed-Ibrahim786@users.noreply.github.com>
2025-06-06 16:20:36 +0530

    Initial commit frontend

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import React, { useRef, useState } from 'react'
+import './App.css'
+import copy_image from "./assets/copy.png"
+import Title from './components/Title/Title'
+import axios from "axios"
+
+
+const App = () => {
+  const longURL = useRef()
+  const alias = useRef()
+  const [aliasError, setAliasError] = useState(false)
+  const [shorturl,setShorturl] = useState('')
+  const [domain, setDomain] = useState('')
+  const [copied,setCopied] = useState(false)
+  const getShortURL = async () =>{
+    
+    try{
+        const data = await axios.post("https://shorten-url-backend-npot.onrender.com",{
+      longURL:longURL.current.value,
+      customAlias:alias.current.value
+    })
+   console.log(data)
+    setShorturl(data.data[0])
+    setDomain(data.data[1])
+    setAliasError(false)
+    }catch(e){
+      if(e.response.status === 409){
+        setAliasError(true)
+      }
+     
+    }
+    
+  }
+
+  const copyToClipboard = async () =>{
+    const fullURL = `${domain}/${shorturl}`
+    try{
+        await navigator.clipboard.writeText(fullURL)
+        setCopied(true)
+    }catch(e){
+      console.error("failed to copy", e)
+    }
+  }
+  
+  
+  return (
+    
+      <div className='fields'>
+      <Title/>
+      <div className="container">
+        <label htmlFor='input'>🔗Shorten a long URL</label>
+        <input onChange={()=>{setCopied(false)}} ref={longURL}  type="text" placeholder='Enter long URL'/>
+
+        <label >🪄Customize your link</label>
+        <input onChange={()=>{setCopied(false)}} ref={alias} type="text" placeholder='Enter your Custom Alias     (optional)'/>
+     
+        {aliasError && (
+          <p className='alias_error'>alias already exist Try other one</p>
+        )}
+        <button className='getlink' onClick={getShortURL}>get link</button>
+        <p className='shorturl' >
+          shortURL : <a href={`${domain}/${shorturl}`} target="_blank">{`${domain}/${shorturl}`}</a>
+          <button style={copied?{outline:"3px solid green"}:{}} onClick={copyToClipboard} className='copy_btn'><img src={copy_image} alt="" /></button>
+        </p>
+      </div>
+      {/* <h4>Number of Clicks:</h4> */}
+      </div>
+      
+ 
+
+    
+    
+  )
+}
+
+export default App
